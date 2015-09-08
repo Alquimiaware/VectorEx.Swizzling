28d8245d3e13655f669e2bda61c9746d6e7db83b
Rafael Muñoz <raudo18@gmail.com>
2015-09-08 18:35:38 +0200

    Added Vector2 XX Operator

---
+++ Assets/Runtime/VectorEx.Swizzling.cs
@@ -0,0 +1,15 @@
+﻿namespace Alquimiaware
+{
+    using UnityEngine;
+    using System.Collections;
+    using System;
+
+    public static class VectorEx
+    {
+        public static Vector2 XX(this Vector2 vector)
+        {
+            vector.y = vector.x;
+            return vector;
+        }
+    }
+}
---
+++ Assets/Tests/Editor/VectorEx.Swizzzling.Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace Alquimiaware
+{
+    using NUnit.Framework;
+    using UnityEngine;
+
+    [TestFixture]
+    public abstract class VectorExSwizzlingTests
+    {
+        public class Vector2Cases : VectorExSwizzlingTests
+        {
+            [Test]
+            public void XX_FirstSecond_ReturnsFirstFirst()
+            {
+                Assert.AreEqual(V2(5, 5), V2(5, 6).XX());
+                Assert.AreEqual(V2(7, 7), V2(7, 5).XX());
+                Assert.AreEqual(V2(0, 0), V2(0, 0).XX());
+            }
+
+            private Vector2 V2(float x, float y)
+            {
+                return new Vector2(x, y);
+            }
+        }
+    }
+}
